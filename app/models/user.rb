3890edb29bf96281a69f8390bbf8cc178af802e4
Jonathan C. da Silva <jonathancsilva@gmail.com>
2015-05-10 23:37:44 -0300

    finalizando cap 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   validates :full_name, :location, presence: true
   validates :bio, length: { minimum: 30 }, allow_blank: false
   validates :password, presence: true, confirmation: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509175430) do
+ActiveRecord::Schema.define(version: 20150511002731) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(version: 20150509175430) do
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
