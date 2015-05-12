fa4d7aeaae95243fce8ca2be07d8bedfe0ca72cd
Jonathan C. da Silva <jonathancsilva@gmail.com>
2015-05-12 20:01:04 -0300

    finalizando cap 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
+  scope "(:locale)", locale: /en|pt/ do
+    resources :rooms
+    resources :users
+  end
 
+  get '/:locale' => 'home#index', locale: /en|pt/
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
